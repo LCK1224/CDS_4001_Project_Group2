c03aae0d9ce8d5547cdf4c8c10db3d652bdf5c20
LCK1224 <leechunkit24@gmail.com>
2025-03-09 00:41:59 +0800

    using OOP

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -9,75 +9,57 @@ def tracker(func):
         print(f"{func.__name__} is processing")
         ret = func(*args, **kwargs)
         end = time.time()
-        print(f"{func.__name__} time used:{end - start}")
+        print(f"time used:{end - start}")
         return ret
     return wrapper
 
 
-@tracker
-def linear_regression(data, label):
-    from sklearn.linear_model import LinearRegression as lr
-    from sklearn.model_selection import train_test_split as tts
+class DataProcessor:
+    def __init__(self, filepath):
+        self.df = pd.read_csv(filepath)
+        self.df = self.df.reset_index()
+        self.train_df = self.df.iloc[3500:]
 
-    X_train, X_test, y_train, y_test = tts(
-        data, label, test_size=0.3, random_state=1224)
-    reg = lr().fit(X_train, y_train)
-    return reg, X_test, y_test
+    @tracker
+    def load_na_row(self, col):
+        return self.df[self.df[col].isna()].index.tolist()
 
+    @tracker
+    def linear_regression(self, data, label):
+        from sklearn.linear_model import LinearRegression as lr
+        from sklearn.model_selection import train_test_split as tts
 
-@tracker
-def load_na_row(df, col):
-    nan_rows = df[df[col].isna()].index.tolist()
-    return nan_rows
+        X_train, X_test, y_train, y_test = tts(
+            data, label, test_size=0.3, random_state=1224)
+        reg = lr().fit(X_train, y_train)
+        return reg, X_test, y_test
 
+    @tracker
+    def fill_missing_values(self, col_name, col_range, label):
+        nan_index = self.load_na_row(col_name)
+        model, _, _ = self.linear_regression(
+            self.train_df.iloc[:, col_range], self.train_df[label])
+        predictions = model.predict(self.df.loc[nan_index].iloc[:, col_range])
+        self.df.loc[nan_index, col_name] = pd.Series(predictions)
 
-@tracker
-def linear_reg_sub(df, train_df, nan_index, col_range, label):
-    model, _, _ = linear_regression(
-        train_df.iloc[:, col_range], train_df[label])
-    prediction = model.predict(df.loc[nan_index].iloc[:, col_range])
-
-    return pd.Series(prediction)
-
-
-@tracker
-def load_data():
-    df = pd.read_csv(r"output.csv")
-    df = df.reset_index()
-    train_df = df.iloc[3500:]
-    nan_index_maxuv = load_na_row(df, "Max UV")
-    nan_index_meanuv = load_na_row(df, "Mean UV")
-    nan_index_windir = load_na_row(df, "Prevailing Wind Direction")
-    nan_index_windspeed = load_na_row(df, "Prevailing Wind Direction")
-    # breakpoint()
-
-    return df, train_df, nan_index_maxuv, nan_index_meanuv, nan_index_windir, nan_index_windspeed
+    @tracker
+    def save_to_csv(self, output_path):
+        self.df.to_csv(output_path)
 
 
 def main():
-    df, train_df, nan_index_maxuv, nan_index_meanuv, nan_index_windir, nan_index_windspeed = load_data()
+    processor = DataProcessor("output.csv")
 
-    # subsitute max uv
-    col_range_maxuv = list(range(1, 15)) + [18]
-    df.loc[nan_index_maxuv, "Max UV"] = linear_reg_sub(
-        df, train_df, nan_index_maxuv, col_range_maxuv, "Max UV")
+    processor.fill_missing_values(
+        "Max UV", list(range(1, 15)) + [18], "Max UV")
+    processor.fill_missing_values(
+        "Mean UV", list(range(1, 16)) + [18], "Mean UV")
+    processor.fill_missing_values(
+        "Wind Speed", list(range(1, 17)) + [18], "Wind Speed")
+    processor.fill_missing_values("Prevailing Wind Direction", list(
+        range(1, 17)) + [18, 19], "Prevailing Wind Direction")
 
-    # subsitute mean uv
-    col_range_meanuv = list(range(1, 16)) + [18]
-    df.loc[nan_index_meanuv, "Mean UV"] = linear_reg_sub(
-        df, train_df, nan_index_meanuv, col_range_meanuv, "Mean UV")
-
-    # subsitute Wind Speed
-    col_range_windspeed = list(range(1, 17)) + [18]
-    df.loc[nan_index_windspeed, "Wind Speed"] = linear_reg_sub(
-        df, train_df, nan_index_windspeed, col_range_windspeed, "Wind Speed")
-
-    # subsitute Prevailing Wind Direction
-    col_range_windir = list(range(1, 17)) + [18] + [19]
-    df.loc[nan_index_windir, "Prevailing Wind Direction"] = linear_reg_sub(
-        df, train_df, nan_index_windir, col_range_windir, "Prevailing Wind Direction")
-
-    df.to_csv(r"output2.csv")
+    processor.save_to_csv("output2.csv")
 
 
 if __name__ == "__main__":
