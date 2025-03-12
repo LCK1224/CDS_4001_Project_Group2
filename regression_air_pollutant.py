afc134f26b7fd02ebd321c50a3223d4231805cf7
unknown <leechunkit24@gmail.com>
2025-03-12 18:27:43 +0800

    modified

--- regression_air_pollutant.py
+++ regression_air_pollutant.py
@@ -5,20 +5,10 @@ def main():
     processor = DataProcessor("merge.csv")
     col_range = list(range(5, 11)) + list(range(12, 22))
     # list(range(5,11)) + list(range(12,22))
-    processor.fill_missing_values(
-        "SO2", col_range, non_zero=True, round_result=True, round_digit=0)
-    processor.fill_missing_values(
-        "NOX", col_range, non_zero=True, round_result=True, round_digit=0)
-    processor.fill_missing_values(
-        "NO2", col_range, non_zero=True, round_result=True, round_digit=0)
-    processor.fill_missing_values(
-        "CO", col_range, non_zero=True, round_result=True, round_digit=0)
-    processor.fill_missing_values(
-        "RSP", col_range, non_zero=True, round_result=True, round_digit=0)
-    processor.fill_missing_values(
-        "O3", col_range, non_zero=True, round_result=True, round_digit=0)
-    processor.fill_missing_values(
-        "FSP", col_range, non_zero=True, round_result=True, round_digit=0)
+    pollutant_lst = ["SO2", "NOX", "NO2", "CO", "RSP", "O3", "FSP"]
+    for p in pollutant_lst:
+        processor.fill_missing_values(
+            p, col_range, non_zero=True, round_result=True, round_digit=0)
     processor.save_to_csv("cleaned_dataset.csv")
 
 
